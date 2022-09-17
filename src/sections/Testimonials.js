62da8c396a62dad8c8905f01ef4d88e5319ed352
sudee404 <sudaise18@gmail.com>
2022-09-18 02:09:27 +0300

    removed unused button

--- src/sections/Testimonials.js
+++ src/sections/Testimonials.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import ButtonLink from "../ui/ButtonLink";
-import OwlDemo from "../ui/OwlDemo";
 export default class Testimonials extends Component {
     render() {
         return (
@@ -8,13 +6,11 @@ export default class Testimonials extends Component {
                 <div className="container-fluid py-5 text-center">
                     <h1 className="display-5 fw-bold">Testimonials</h1>
                     <p className="fs-4 ">
-                        Below is where I'll show all the reviews given by my clients.
-                        
+                        Below is where I'll show all the reviews given by my
+                        clients.
                     </p>
-                    <div className="text-center me-5 pe-5 py-2"><ButtonLink text="Leave a Review"/></div>
-                    
+                    {/* <div className="text-center me-5 pe-5 py-2"><ButtonLink text="Leave a Review"/></div> */}
                 </div>
-                <OwlDemo />
             </div>
         );
     }
